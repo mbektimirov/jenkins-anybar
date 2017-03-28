9c112dd8de1740ce25f536bd57dd2a63aafdc053
mbektimirov <mbektimirov@gmail.com>
2017-03-28 23:29:38 +0300

    Drone test

---
+++ test/dumb-test.js
@@ -0,0 +1,15 @@
+var expect = require("chai").expect;
+
+describe("Calc tests", function() {
+  it("sums two values", function() {
+    expect(1 + 1).to.equal(2);
+  });
+
+  it("multiplies two values", function() {
+    expect(2 * 3).to.equal(6);
+  });
+
+  it("subtracts value", function() {
+    expect(2 - 3).to.equal(0);
+  });
+});
